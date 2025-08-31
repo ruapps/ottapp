e8afe7c31d7cd8a08d66aebb25c0cd49cda03f1e
ruapps <rahulupdhyy28@gmail.com>
2025-08-31 16:27:58 +0530

    a minor change

--- src/Api/loadapi.js
+++ src/Api/loadapi.js
@@ -4,10 +4,10 @@ import axios from "axios";
 const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 const fetchMovies = createAsyncThunk("movie/getMovies", async () => {
-  const response = await axios.get(`${BASE_URL}'/movies'`);
+  const response = await axios.get(`${BASE_URL}/movies`);
   console.log(BASE_URL, "movies:", response);
 
-  return response.data.movies;
+  return response.data;
 });
 
 export { fetchMovies };
