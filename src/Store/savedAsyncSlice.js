c6337ba1d844f172b8abac6987dcf32299d5befc
ruapps <rahulupdhyy28@gmail.com>
2025-09-09 18:34:29 +0530

    added synce slice

---
+++ src/Store/savedAsyncSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { saveMovie, deleteMovie } from "../Api/savedApi";
+
+const savedSlice = createSlice({
+  name: "saved",
+  initializer: { status: false, items: [], flag: {} },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(saveMovie.fulfilled, (state, action) => {
+        if (action.payload) {
+          state.items.push(action.payload);
+          state.flag[action.payload.id] = !state.flag[action.payload.id];
+          if (!state.status) state.status = true;
+          // console.log(state.flag);
+        }
+      })
+      .addCase(saveMovie.rejected, (state, action) => {
+        // state.items = action.payload;
+        // console.log(state.items)
+      })
+      .addCase(deleteMovie.fulfilled, (state, action) => {
+        delete state.flag[action.payload];
+        state.items = state.items.filter((item) => item.id !== action.payload);
+        if (state.items.length === 0) state.status = false;
+        // console.log(action.payload.id);
+        return state;
+      })
+      .addCase(deleteMovie.rejected, (state, action) => {
+        // state.items = action.payload;
+        // console.log(state.items)
+      })
+
+      .addDefaultCase((state) =>
+        !state ? (state = { status: false, items: [], flag: {} }) : state
+      );
+  },
+});
+
+export default savedSlice.reducer;
--- src/Store/savedSlice.js
+++ src/Store/savedSlice.js
@@ -1,41 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { saveMovie, deleteMovie } from "../Api/savedApi";
 
 const savedSlice = createSlice({
   name: "saved",
   initializer: { status: false, items: [], flag: {} },
+  reducers: {
+    addItem: (state, action) => {
+      state.items.push(action.payload);
+      state.flag[action.payload.id] = true;
+      state.status = true;
+    },
+    delItem: (state, action) => {
+      delete state.flag[action.payload];
+      state.items = state.items.filter((item) => item.id !== action.payload);
+      if (state.items.length === 0) state.status = false;
+    },
+  },
 
   extraReducers: (builder) => {
-    builder
-      .addCase(saveMovie.fulfilled, (state, action) => {
-        if (action.payload) {
-          state.items.push(action.payload);
-          state.flag[action.payload.id] = !state.flag[action.payload.id];
-          if (!state.status) state.status = true;
-          // console.log(state.flag);
-        }
-      })
-      .addCase(saveMovie.rejected, (state, action) => {
-        // state.items = action.payload;
-        // console.log(state.items)
-      })
-      .addCase(deleteMovie.fulfilled, (state, action) => {
-        delete state.flag[action.payload];
-        state.items = state.items.filter((item) => item.id !== action.payload);
-        if (state.items.length === 0) state.status = false;
-        // console.log(action.payload.id);
-        return state;
-      })
-      .addCase(deleteMovie.rejected, (state, action) => {
-        // state.items = action.payload;
-        // console.log(state.items)
-      })
-
-      .addDefaultCase((state) =>
-        !state ? (state = { status: false, items: [], flag: {} }) : state
-      );
+    builder.addDefaultCase((state) =>
+      !state ? (state = { status: false, items: [], flag: {} }) : state
+    );
   },
 });
 
-export const { addItems, delItems } = savedSlice.actions;
+export const { addItem, delItem } = savedSlice.actions;
 export default savedSlice.reducer;
