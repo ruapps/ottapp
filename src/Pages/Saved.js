4f593d5bbbdbb0000b6830fd2e52e00272764efb
ruapps <rahulupdhyy28@gmail.com>
2025-09-11 19:17:52 +0530

    added carousel to saved comp

--- src/Pages/Saved.js
+++ src/Pages/Saved.js
@@ -1,19 +1,23 @@
 import { useDispatch, useSelector } from "react-redux";
 import Listitems from "../Components/Listitems";
-import { deleteMovie } from "../Api/savedApi";
+import { delItem } from "../Store/savedSlice";
 
 const Saved = () => {
   const { status, items } = useSelector((state) => state.saved);
+  const carouseItemInd = useSelector((state) => state.carousel);
+
   const dispatch = useDispatch();
+  console.log("saved called");
 
   const handleDelete = (id) => {
-    dispatch(deleteMovie(id));
+    dispatch(delItem(id));
   };
 
   return (
     <Listitems
       movies={items}
       status={status}
+      carouseItemInd={carouseItemInd}
       compName="saved"
       onDelete={handleDelete}
       text={"No items saved yet"}
