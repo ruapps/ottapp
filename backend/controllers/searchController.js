a4833afb6ea3d59ed6ee84ea0d6416a81723fb55
ruapps <rahulupdhyy28@gmail.com>
2026-06-12 19:55:53 +0530

    Upgrade Semantic Search Feature, upgrade searchController with  synonyms dictionary and text index search

--- backend/controllers/searchController.js
+++ backend/controllers/searchController.js
@@ -1,5 +1,6 @@
 const Movie = require("../models/movie");
 const asyncHandler = require("../utils/asyncHandler");
+const synonyms = require("../utils/synonyms");
 
 exports.searchMovies = asyncHandler(async (req, res) => {
   const query = req.query.q?.trim();
@@ -8,10 +9,26 @@ exports.searchMovies = asyncHandler(async (req, res) => {
     return res.status(200).json([]);
   }
 
+  const words = query.toLowerCase().split(" ");
+
+  const expandedWords = new Set();
+
+  words.forEach((word) => {
+    expandedWords.add(word);
+
+    if (synonyms[word]) {
+      synonyms[word].forEach((term) => {
+        expandedWords.add(term);
+      });
+    }
+  });
+
+  const searchQuery = Array.from(expandedWords).join(" ");
+
   const movies = await Movie.find(
     {
       $text: {
-        $search: query,
+        $search: searchQuery,
       },
     },
     {
---
+++ backend/utils/synonyms.js
@@ -0,0 +1,15 @@
+module.exports = {
+  wizard: ["sorcerer", "magic", "mage"],
+
+  magic: ["wizard", "sorcerer", "mystic"],
+
+  assassin: ["hitman", "killer", "revenge"],
+
+  superhero: ["marvel", "hero", "avenger"],
+
+  lion: ["animal", "king", "jungle"],
+
+  jungle: ["forest", "wild", "animal"],
+
+  anime: ["animation", "cartoon"],
+};
